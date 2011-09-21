5d7a042e9ac0f43e6ee2f3a62153c2ec474b47e8
Nerian <siotopo@gmail.com>
2011-09-21 19:38:24 +0200

    Maps

--- lib/bloodchalice.rb
+++ lib/bloodchalice.rb
@@ -1,5 +1,37 @@
 class BloodChalice  
   def self.run
-    puts 'Welcome to Blood Chalice'
+    puts 'Welcome to Blood Chalice'  
+    game = Game.new        
+  end
+
+  class Game
+
+    def initialize()
+      @map = generate_map('map1')
+      show_map
+    end    
+    
+    def show_map
+      @map.each do |line|
+        line.each do |tile|
+          print tile
+        end    
+        puts ''
+      end
+    end                    
+
+    def generate_map(map_name)
+      map = []
+      f = File.open("lib/world/#{map_name}", "r") 
+      i = 0  
+      f.each_line do |line|         
+        map[i] = []
+        line.strip.each_char do |tile|          
+          map[i].push tile
+        end                
+        i += 1
+      end
+      map
+    end
   end
 end
