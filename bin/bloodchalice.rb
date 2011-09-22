d514853f0294b0898f201eb60c18b36eec9d7c4d
Nerian <siotopo@gmail.com>
2011-09-22 15:00:17 +0200

    Aplication instead of bare bone run

--- bin/bloodchalice.rb
+++ bin/bloodchalice.rb
@@ -1,3 +1,3 @@
 require_relative '../lib/bloodchalice'
 
-BloodChalice.run
+BloodChalice::Application.run
--- lib/bloodchalice/bloodchalice.rb
+++ lib/bloodchalice/bloodchalice.rb
@@ -1,12 +1,14 @@
-class BloodChalice
-  def self.run
-    puts 'Welcome to Blood Chalice'
-    puts "How many players are going to play?"
-    number_of_players = gets
-    game = Game.new(number_of_players: number_of_players.strip.to_i)
-    puts "There are #{game.players.size} players"    
-    while not game.end_of_game?
-      game.next_turn
+class BloodChalice    
+  class Application
+    def self.run
+      puts 'Welcome to Blood Chalice'
+      puts "How many players are going to play?"
+      number_of_players = gets.strip.to_i
+      game = Game.new(number_of_players: number_of_players)
+      puts "There are #{game.players.size} players"    
+      while not game.end_of_game?
+        game.next_turn
+      end
     end
-  end
+  end  
 end
